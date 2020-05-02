1c91cff63d78f0dde4bc278a9abff1f1f919d16e
CarolinaSantejo <a89500@alunos.uminho.pt>
2020-05-02 15:57:22 +0100

    Teste

--- src/Conta.java
+++ src/Conta.java
@@ -102,7 +102,7 @@ public class Conta implements Comparable {
     //ToString
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        sb.append("Código da conta: '").append(this.codigo).append("'\n");
+        sb.append("CÃ³digo da conta: '").append(this.codigo).append("'\n");
         sb.append("Nome da conta: '").append(this.nome).append("'\n");
         sb.append("Coordenadas: ").append(this.gps.toString()).append("\n");
         sb.append("Email: '").append(this.email).append("'\n");
